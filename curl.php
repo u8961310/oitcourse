5ffdff6b67432561b538bc65ac6ba5be40fa42eb
Cake Chen <u8961310@gmail.com>
2014-12-20 23:48:05 +0800

    modify php parser into array

--- curl.php
+++ curl.php
@@ -1,59 +1,55 @@
 <?php
-include("lib/LIB_http.php");
-include("lib/LIB_parse.php");
-$course_array = array();
-$course_count = 0;
-//$syear = '1031'; //學期
-$target = "http://info.oit.edu.tw/cosinfo/cos/show_cos_info.asp?mSmtr=1031&dept_no=MI&sel_type=1";
-$web_page = http_get($target,"");
-$table_array = parse_array($web_page["FILE"],"<table","</table>");
-// for($xx=0;$xx<count($table_array);$xx++){
-	
-// }
-$course_row_array = parse_array($table_array[0],"<tr ","</tr>");
-//print_r($course_row_array[0]);
-print_r($course_row_array[1]);
-//echo stristr($course_row_array[1], "學期");
-echo stristr($course_row_array[1], "1031");
-//print_r($course_row_array);
-for($table_row=0;$table_row<count($course_row_array);$table_row++){
-	
-	$heading_landmark = "開課系所";
-	if(stristr($course_row_array[$table_row],"學期")){
-		echo "FOUND:Table heading row\n";
-		$table_cell_array = parse_array($course_row_array[$table_row],"<td","</td>");
-		for($heading_cell=0;$heading_cell < count($table_cell_array); $heading_cell++){
-			if(stristr(strip_tags(trim($table_cell_array[$heading_cell])),"開課系所"))
-				$syear_coumn = $heading_cell;
-			if(stristr(strip_tags(trim($table_cell_array[$heading_cell])),"課目代號"))
- 				$course_id_coumn = $heading_cell;
+include "simple_html_dom.php";
+//$url = 'http://info.oit.edu.tw/cosinfo/cos/show_cos_info.asp?mSmtr=1031';
+$url = 'http://info.oit.edu.tw/cosinfo/cos/show_cos_info.asp?mSmtr=1031&dept_no=MI&sel_type=1';
+$cURL = curl_init();
+curl_setopt($cURL, CURLOPT_URL, $url); 
+curl_setopt($cURL, CURLOPT_HEADER,0); 
 
-		}
-		
+curl_setopt($cURL, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($cURL, CURLOPT_COOKIESESSION, true); 
+curl_setopt($cURL, CURLOPT_FOLLOWLOCATION, 1); 
+$page = curl_exec($cURL); 
+curl_close($cURL); 
 
-		$heading_row = $table_row;
+$html = str_get_html($page);
+$data = array();
 
-		$ending_landmark = "樓館代碼說明 - [第1碼] 1：有庠； 2：誠勤； 3：元智； 5：教學； 6：實習；";
-		if((stristr($course_row_array[$table_row],$ending_landmark))){
-			echo "PARSING COMPLETE\n";
-			break;
-		}
-		if(isset($heading_row) && $heading_row<$table_row){
-			$table_cell_array = parse_array($course_row_array[$table_row],"<td","</td>");
-			$course_array[$course_count]['syear']  = strip_tags(trim($table_cell_array[$syear_coumn]));
-
-			$course_count++;
-			echo "Item Count $course_count\n";
-		}
-		for($xx=0;$xx<count($course_array);$xx++){
-			echo "$xx. ";
-			echo "syear:".$course_array[$course_count]['syear'];
-		}
-
+foreach ($html->find('tr') as $row) {
+	$rowData = array();
+	foreach($row->find('td')as $cell){
+		$rowData[]= $cell->innertext;
 	}
-	//print_r($course_row_array);
+	$theData[] = $rowData;
 }
 
+for($i=0;$i<count($theData);$i++){
+	for($j=0;$j<count($theData[$i]);$j++){
+		$theData[$i][$j] = str_replace("<div align=\"center\"><span class=\"contact\">樓館代碼說明 - [第1碼] 1：有庠； 2：誠勤； 3：元智； 5：教學； 6：實習；</span></div>",null, $theData[$i][$j]);
+		$theData[$i][$j] = str_replace("<div align=\"center\"><span class=\"contact\">[第2,3碼] 樓層</span></div>",null, $theData[$i][$j]);
+		$theData[$i][$j] = str_replace("<div align=\"center\"><span class=\"contact\">[第4,5碼] 教室流水號</span></div>",null, $theData[$i][$j]);
+		$theData[$i][$j] = str_replace("<div align=\"center\">        <input name=\"Submit\" type=\"button\" class=\"btm\" onclick=\"MM_callJS('javascript:history.go(-1);')\" value=\"回上一頁\">      </div>",null, $theData[$i][$j]);
+		$theData[$i][$j] = strip_tags($theData[$i][$j]);
 
+ }
+	}
+//有bug
+for($i=0;$i<count($theData);$i++){
+	for($j=0;$j<count($theData[$i]);$j++){
+		if(empty($theData[$i][$j])){
+			//echo "第$i $j 項刪除<br />\n";
+			unset($theData[$i][$j]);
+		 }
+ }
+	}
+	$theData = array_values($theData);
+for($i=0;$i<count($theData);$i++){
+	if(empty($theData[$i])){
+		 	echo "第 $i 項刪除<br />\n";
+			unset($theData[$i]);
+		 }
+	}
+
+print_r($theData );
 
 ?>
